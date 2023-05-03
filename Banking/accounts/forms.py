26b064b33a7147bbccc531594be4c719ff08fba3
vanditpopat <51928264+vanditpopat@users.noreply.github.com>
2023-05-03 18:18:26 -0400

    milestone 3

--- Banking/accounts/forms.py
+++ Banking/accounts/forms.py
@@ -1,8 +1,9 @@
 import datetime
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SelectField, SubmitField, DateField, FloatField
+from wtforms import TextAreaField, SelectField, SubmitField, DateField, FloatField, StringField
 from wtforms.validators import DataRequired, NumberRange, InputRequired
 
+#v645 Date 05/02/2023
 class CreateAccountForm(FlaskForm):
     acc_type_choices = [('Checking', 'Checking Account'), ('Savings', 'Savings Account'), ('Fixed Deposit', 'Fixed Deposit Account'), ('Money Market', 'Money Market Account'), ('Certificates of Deposit', 'Certificates of Deposit Account')]
     acc_type = SelectField("Account Type", choices=acc_type_choices, validators=[DataRequired()])
@@ -15,6 +16,7 @@ class CreateAccountForm(FlaskForm):
         if not self.creation_date.data:
             self.creation_date.data = datetime.date.today()
 
+#v645 Date 05/02/2023
 class DepositWithdrawForm(FlaskForm):
     account = SelectField("Account", choices=[], validators=[DataRequired()])
     funds = FloatField("Funds", validators=[InputRequired(),NumberRange(min=1)])
@@ -25,3 +27,30 @@ class DepositWithdrawForm(FlaskForm):
         super().__init__()  # calls the base initialisation and then...
         if accounts: 
             self.account.choices = [(c['id'], f"{c['account_type']} Account - {c['account_number']}") for c in accounts]
+
+#v645 Date 05/02/2023
+class TransferForm(FlaskForm):
+    account_src = SelectField("Account Source", choices=[], validators=[DataRequired()])
+    account_dest = SelectField("Account Destination", choices=[], validators=[DataRequired()])
+    funds = FloatField("Funds", validators=[InputRequired(),NumberRange(min=1)])
+    memo = TextAreaField ("Memo", validators=[])
+    submit = SubmitField("Submit")
+
+    def __init__(self, accounts=None):
+        super().__init__()  # calls the base initialisation and then...
+        if accounts: 
+            self.account_src.choices = [(c['id'], f"{c['account_type']} Account - {c['account_number']} - ${c['balance']}") for c in accounts]
+            self.account_dest.choices = [(c['id'], f"{c['account_type']} Account - {c['account_number']} - ${c['balance']}") for c in accounts]
+#v645 Date 05/02/2023
+class ExtTransferForm(FlaskForm):
+    account_src = SelectField("Account Source", choices=[], validators=[DataRequired()])
+    last_name = StringField("Last Name")
+    account_dest = StringField("Last 4 digits of destination user’s account number")
+    funds = FloatField("Funds", validators=[InputRequired(),NumberRange(min=1)])
+    memo = TextAreaField ("Memo", validators=[])
+    submit = SubmitField("Submit")
+
+    def __init__(self, accounts=None):
+        super().__init__()  # calls the base initialisation and then...
+        if accounts: 
+            self.account_src.choices = [(c['id'], f"{c['account_type']} Account - {c['account_number']} - ${c['balance']}") for c in accounts]
